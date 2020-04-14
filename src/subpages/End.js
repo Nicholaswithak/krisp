f6c2728f3cb5a94f80a3de81e77efd96465869ca
NicholaswithAK <ncl1984@gmail.com>
2020-04-13 18:08:25 -0700

    added forms

--- src/subpages/End.js
+++ src/subpages/End.js
@@ -8,32 +8,26 @@ export default function End() {
       style={{ backgroundColor: "#7E57C2", color: "white" }}
     >
       <div className="container">
-        <div className="flexbox">
-          <Fade left>
-            <h1 className="text-center my-5">Stay in touch</h1>
-            <div className="row my-4">
-              <div className="col-lg-6 mb-4">
-                <input
-                  type="name"
-                  className="inputBox"
-                  placeholder="Your Name"
-                />
-              </div>
-              <div className="col-lg-6 mb-4">
-                <input
-                  type="email"
-                  className="inputBox"
-                  placeholder="Your Email"
-                />
-              </div>
-            </div>
-            <textarea rows = "2" className="inputBox mb-4" placeholder="Your Message" />
-            <button className="btn btn-light mb-4 py-3">
-              Submit
-            </button>
-          </Fade>
+
+      <form method="post" netlify-honeypot="bot-field" data-netlify="true">
++       <input type="hidden" name="bot-field" />
+              <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+              </p>
+
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+
         </div>
       </div>
-    </div>
+
   )
 }
